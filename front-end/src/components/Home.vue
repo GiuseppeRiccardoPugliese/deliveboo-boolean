1cba3f2a24a1a34fd04c994425bed668af05b76e
Massidama <massidama@outlook.it>
2024-03-12 12:26:03 +0100

    container fluid footer

--- front-end/src/components/Home.vue
+++ front-end/src/components/Home.vue
@@ -7,13 +7,45 @@ export default {
 </script>
 
 <template>
-    
+    <div class="container-fluid">
+        <div class="jumbotron">
+            <div class="jumbo-title">
+                <span> I PIATTI CHE AMI DOVE E QUANDO VUOI </span>
+            </div>
+            <div class="advanced-search">
+                <div class="input-group">
+                    <input type="text" placeholder="Inserisci la tua città..">
+                    <button type="button"> Cerca </button>
+                </div>
+            </div>
+
+        </div>
+    </div>
 </template>
 
 <style scoped lang="scss">
-    div{
-        height: 30px;
-        background-color: pink;
+    .container-fluid {
+       width: 100%;
+       height: 500px;
+       background-image: url(../../public/img/jumbotron1.jpg);
+       background-size: cover;
+
+       .jumbotron {
+
+            span {
+                color: white;
+                font-size: 30px;
+            }
+
+            .advanced-search {
+
+                button {
+                    border-radius: 100px;
+                    padding: 16px 40px;
+                }
+            }
+       }
+       
     }
 
 
