30d3911dcaabc13b5a7e870f1470ad4acefcde44
CosimoA <cosimo.archip@gmail.com>
2024-03-12 13:27:41 +0100

    FEATR: add responsive restaurants cards

--- front-end/src/components/Restaurants.vue
+++ front-end/src/components/Restaurants.vue
@@ -1,19 +1,44 @@
 <script>
-
 export default {
   name: 'Restaurants',
-}
-
+};
 </script>
 
-
 <template>
-  <div class="aaa">
-      LISTA RISTORANTI
-  </div>
+  <section>
+    <div class="my-container">
+      <div class="back-arrow">
+        <--
+      </div>
+      <span>
+        Ristoranti a Milano
+      </span>
+
+      <div class="my-card-container">
+        <div class="my-card">
+          
+        </div>        
+        <div class="my-card">
+          
+        </div>        
+        <div class="my-card">
+          
+        </div>        
+        <div class="my-card">
+          
+        </div>
+        <div class="my-card">
+          
+        </div>
+        <div class="my-card">
+          
+        </div>
+      </div>
+    </div>
+  </section>
+
 </template>
 
 <style scoped lang="scss">
-
-
+  @import '../style/restaurants.scss';
 </style>
