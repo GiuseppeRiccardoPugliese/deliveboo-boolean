7319f026b2cd462001d661a5fff9289ab83b5c6a
CosimoA <cosimo.archip@gmail.com>
2024-03-12 16:08:41 +0100

    FEATR: cycle cards and pull details from server.

--- front-end/src/components/Restaurants.vue
+++ front-end/src/components/Restaurants.vue
@@ -1,43 +1,69 @@
 <script>
 export default {
   name: 'Restaurants',
+  data() {
+    return {
+      // Array per memorizzare i dati dei ristoranti
+      ristoranti: [] 
+    };
+  },
+  mounted() {
+    // Chiamata per recuperare i dati dei ristoranti
+    this.fetchData(); 
+  },
+  methods: {
+    fetchData() {
+      // Simulazione della chiamata API
+      fetch('/server.json')
+        .then(response => response.json())
+        .then(data => {
+          // Imposta i dati dei ristoranti ottenuti dalla chiamata API
+          this.ristoranti = data;
+        })
+        .catch(error => {
+          console.error('Si è verificato un errore:', error);
+        });
+    },
+    goBack() {
+      // Funzione per tornare alla pagina precedente
+      this.$router.go(-1);
+    }
+  }
 };
 </script>
 
 <template>
   <section>
     <div class="my-container">
-      <div class="back-arrow">
-        <--
+
+      <div class="arrow-container">
+        <span class="back-arrow">
+          <!-- Evento click per tornare indietro -->
+          <i @click="goBack" class="fa-solid fa-circle-left"></i>
+        </span>
+        <span class="restaurants-zone">
+          <strong>Ristoranti a Milano</strong>
+        </span>
       </div>
-      <span>
-        Ristoranti a Milano
-      </span>
 
       <div class="my-card-container">
-        <div class="my-card">
-          
+        <!-- Utilizza v-for per iterare sui ristoranti e mostrare i dati -->
+        <div class="my-card" v-for="(ristorante, index) in ristoranti" :key="index">
+
+          <router-link :to="'/details/' + index" class="router-link">
+            <div class="restaurant-image" :style="{ backgroundImage: 'url(' + ristorante.immagine + ')' }"></div>
+            <h6>{{ ristorante.nome }}</h6>
+            <p v-if="ristorante.aperto" class="open-status">Aperto</p>
+            <p v-else class="closed-status">Chiuso</p>
+          </router-link>
+
         </div>        
-        <div class="my-card">
-          
-        </div>        
-        <div class="my-card">
-          
-        </div>        
-        <div class="my-card">
-          
-        </div>
-        <div class="my-card">
-          
-        </div>
-        <div class="my-card">
-          
-        </div>
       </div>
+
     </div>
   </section>
-
 </template>
+
 
 <style scoped lang="scss">
   @import '../style/restaurants.scss';
