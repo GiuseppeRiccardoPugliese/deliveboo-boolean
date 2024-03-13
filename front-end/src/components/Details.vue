e71f36d4153c662ba87ed7f5c449583fdaab3608
Davide Prencipe <davidedev04@gmail.com>
2024-03-13 22:00:58 +0100

    .

--- front-end/src/components/Details.vue
+++ front-end/src/components/Details.vue
@@ -9,8 +9,6 @@ export default {
     mounted() {
         // Chiamata per recuperare i dati dei ristoranti
         this.fetchData();
-        console.log(this.$route.params.index);
-        console.log(this.restaurants);
     },
     methods: {
         fetchData() {
@@ -71,43 +69,43 @@ export default {
         </div>
         <div class="menu text-center border-top">
             <h1 class="mt-4">MENU</h1>
-            <h2 v-if="restaurants[this.$route.params.index].menu.Antipasti" class="mt-5">ANTIPASTI:</h2>
+            <h2 v-if="restaurants[this.$route.params.index].menu.Antipasti" class="mt-5 fw-bold">ANTIPASTI:</h2>
             <div v-for="(plate,i) in restaurants[this.$route.params.index].menu.Antipasti" :key="i" class="mt-4 d-flex flex-column">
                 <h5>{{ plate.nome }}</h5>
                 <span>{{ plate.descrizione }}</span>
                 <strong>{{ plate.prezzo }}€</strong>
             </div>
-            <h2 v-if="restaurants[this.$route.params.index].menu.PrimiPiatti" class="mt-5">PRIMI PIATTI:</h2>
+            <h2 v-if="restaurants[this.$route.params.index].menu.PrimiPiatti" class="mt-5 fw-bold">PRIMI PIATTI:</h2>
             <div v-for="(plate,i) in restaurants[this.$route.params.index].menu.PrimiPiatti" :key="i" class="mt-4 d-flex flex-column">
                 <h5>{{ plate.nome }}</h5>
                 <span>{{ plate.descrizione }}</span>
                 <strong>{{ plate.prezzo }}€</strong>
             </div>
-            <h2 v-if="restaurants[this.$route.params.index].menu.SecondiPiatti" class="mt-5">SECONDI PIATTI:</h2>
+            <h2 v-if="restaurants[this.$route.params.index].menu.SecondiPiatti" class="mt-5 fw-bold">SECONDI PIATTI:</h2>
             <div v-for="(plate,i) in restaurants[this.$route.params.index].menu.SecondiPiatti" :key="i" class="mt-4 d-flex flex-column">
                 <h5>{{ plate.nome }}</h5>
                 <span>{{ plate.descrizione }}</span>
                 <strong>{{ plate.prezzo }}€</strong>
             </div>
-            <h2 v-if="restaurants[this.$route.params.index].menu.Pizze" class="mt-5">PIZZE:</h2>
+            <h2 v-if="restaurants[this.$route.params.index].menu.Pizze" class="mt-5 fw-bold">PIZZE:</h2>
             <div v-for="(plate,i) in restaurants[this.$route.params.index].menu.Pizze" :key="i" class="mt-4 d-flex flex-column">
                 <h5>{{ plate.nome }}</h5>
                 <span>{{ plate.descrizione }}</span>
                 <strong>{{ plate.prezzo }}€</strong>
             </div>
-            <h2 v-if="restaurants[this.$route.params.index].menu.SushiESashimi" class="mt-5">SUSHI:</h2>
+            <h2 v-if="restaurants[this.$route.params.index].menu.SushiESashimi" class="mt-5 fw-bold">SUSHI:</h2>
             <div v-for="(plate,i) in restaurants[this.$route.params.index].menu.SushiESashimi" :key="i" class="mt-4 d-flex flex-column">
                 <h5>{{ plate.nome }}</h5>
                 <span>{{ plate.descrizione }}</span>
                 <strong>{{ plate.prezzo }}€</strong>
             </div>
-            <h2 v-if="restaurants[this.$route.params.index].menu.Dolci" class="mt-5">DOLCI:</h2>
+            <h2 v-if="restaurants[this.$route.params.index].menu.Dolci" class="mt-5 fw-bold">DOLCI:</h2>
             <div v-for="(plate,i) in restaurants[this.$route.params.index].menu.Dolci" :key="i" class="mt-4 d-flex flex-column">
                 <h5>{{ plate.nome }}</h5>
                 <span>{{ plate.descrizione }}</span>
                 <strong>{{ plate.prezzo }}€</strong>
             </div>
-            <h2 v-if="restaurants[this.$route.params.index].menu.Bevande" class="mt-5">BEVANDE:</h2>
+            <h2 v-if="restaurants[this.$route.params.index].menu.Bevande" class="mt-5 fw-bold">BEVANDE:</h2>
             <div v-for="(plate,i) in restaurants[this.$route.params.index].menu.Bevande" :key="i" class="mt-4 d-flex flex-column">
                 <h5>{{ plate.nome }}</h5>
                 <span>{{ plate.descrizione }}</span>
