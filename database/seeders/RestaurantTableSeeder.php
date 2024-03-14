21bc1e95901e9a2612648e9849015f87ad9ecfbe
Giuseppe Riccardo Pugliese <giuseppe.riccardo.pugliese@gmail.com>
2024-03-14 16:00:14 +0100

    Popolamento DB per i piatti con le relative chiavi restaurant_id, ristoranti con lo user_id & user

--- database/seeders/RestaurantTableSeeder.php
+++ database/seeders/RestaurantTableSeeder.php
@@ -2,6 +2,8 @@
 
 namespace Database\Seeders;
 
+use App\Models\Restaurant;
+use App\Models\User;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
@@ -14,6 +16,101 @@ class RestaurantTableSeeder extends Seeder
      */
     public function run()
     {
-        //
+        $restaurants = [
+            [
+                'piva' => '01234567890',
+                'address' => 'Via Roma, 1',
+                'image' => 'image1.jpg',
+                'visible' => true,
+            ],
+            [
+                'piva' => '09876543210',
+                'address' => 'Corso Italia, 15',
+                'image' => 'image2.jpg',
+                'visible' => true,
+            ],
+            [
+                'piva' => '11223344557',
+                'address' => 'Piazza Garibaldi, 10',
+                'image' => 'image3.jpg',
+                'visible' => false,
+            ],
+            [
+                'piva' => '11223344556',
+                'address' => 'Via Garibaldi, 20',
+                'image' => 'image4.jpg',
+                'visible' => true,
+            ],
+            [
+                'piva' => '98765432101',
+                'address' => 'Corso Vittorio Emanuele, 5',
+                'image' => 'image5.jpg',
+                'visible' => true,
+            ],
+            [
+                'piva' => '54321678909',
+                'address' => 'Via Dante Alighieri, 30',
+                'image' => 'image6.jpg',
+                'visible' => false,
+            ],
+            [
+                'piva' => '65432109870',
+                'address' => 'Piazza Duomo, 1',
+                'image' => 'image7.jpg',
+                'visible' => true,
+            ],
+            [
+                'piva' => '09876543211',
+                'address' => 'Via Milano, 12',
+                'image' => 'image8.jpg',
+                'visible' => false,
+            ],
+            [
+                'piva' => '76543210987',
+                'address' => 'Corso Cavour, 8',
+                'image' => 'image9.jpg',
+                'visible' => true,
+            ],
+            [
+                'piva' => '54321098765',
+                'address' => 'Piazza Repubblica, 25',
+                'image' => 'image10.jpg',
+                'visible' => false,
+            ],
+        ];
+
+        // Restaurant::count(10)
+        //     ->make() //NELLA relazione ONE to MANY si utilizza il make()
+        //             //NELLA relazione MANY to MANY si utilizza il create
+        //     ->each(function ($restaurant) {
+
+        //         $user = User::inRandomOrder()->first();
+        //         $event -> user() -> associate($user);
+
+        //         $event->save();
+
+        //         $tags = Tag::inRandomOrder()->take(3)->get();
+        //         $event->tags()->attach($tags);
+        //     });
+
+        $users = User::inRandomOrder()->get();
+
+        foreach ($restaurants as $restaurant) {
+
+            $new_restaurant = new Restaurant();
+
+            // Prendi un utente casuale
+            $user = $users->shift(); // Rimuovi l'utente dalla lista per evitare il riassegnamento
+
+            $new_restaurant->user()->associate($user);
+
+            $new_restaurant->piva = $restaurant['piva'];
+            $new_restaurant->address = $restaurant['address'];
+            $new_restaurant->image = $restaurant['image'];
+            $new_restaurant->visible = $restaurant['visible'];
+
+            //Salvo
+            $new_restaurant->save();
+        }
     }
 }
