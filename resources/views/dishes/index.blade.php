d4faa7edd44c1e0abd03dcb9ff2bbe29509550b7
Giuseppe Riccardo Pugliese <giuseppe.riccardo.pugliese@gmail.com>
2024-03-15 04:44:39 +0100

    Autorizzazioni delle operazioni CRUD nell'index riguardanti le dishes

--- resources/views/dishes/index.blade.php
+++ resources/views/dishes/index.blade.php
@@ -1,31 +1,42 @@
 @extends('layouts.app')
 @section('content')
-    <h1>I miei piatti: </h1>
+    @unless (Auth::check())
+        <h1>Ecco la lista di tutti i piatti: </h1>
+    @endunless
 
-    {{-- CREATE --}}
     @auth
+        <h1>Crea i tuoi piatti</h1>
+
+        {{-- CREATE --}}
         <a href="{{ route('dish.create') }}">CREATE</a>
     @endauth
 
     <ul>
         @foreach ($dishes as $dish)
-            <li>
-                {{-- SHOW --}}
-                <a href="{{ route('dish.show', $dish->id) }}">
+            @if (Auth::check())
+                @if (Auth::user()->id == $dish->restaurant_id)
+                    <li>
+                        {{-- SHOW --}}
+                        <a href="{{ route('dish.show', $dish->id) }}">
+                            {{ $dish->name }}
+                        </a>
+                    </li>
+                    {{-- EDIT --}}
+                    <a href="{{ route('dish.edit', $dish->id) }}">EDIT</a>
+
+                    {{-- DELETE --}}
+                    <form action="{{ route('dish.destroy', $dish->id) }}" method="POST">
+                        @csrf
+                        @method('DELETE')
+
+                        <input type="submit" id="destroy" name="destroy" value="DELETE">
+                    </form>
+                @endif
+            @else
+                <li>
                     {{ $dish->name }}
-                </a>
-            </li>
-            {{-- EDIT --}}
-            <a href="{{ route('dish.edit', $dish->id) }}">EDIT</a>
-
-            {{-- DELETE --}}
-
-            <form action="{{ route('dish.destroy', $dish->id) }}" method="POST">
-                @csrf
-                @method('DELETE')
-
-                <input type="submit" id="destroy" name="destroy" value="DELETE">
-            </form>
+                </li>
+            @endif
         @endforeach
     </ul>
 @endsection
