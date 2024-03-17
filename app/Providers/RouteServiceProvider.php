74563543982e6da60b112ce80a7269abd8c6e92f
Giuseppe Riccardo Pugliese <giuseppe.riccardo.pugliese@gmail.com>
2024-03-17 20:45:41 +0100

    Dopo la login ti riporta sulla pagina dei piatti

--- app/Providers/RouteServiceProvider.php
+++ app/Providers/RouteServiceProvider.php
@@ -17,7 +17,7 @@ class RouteServiceProvider extends ServiceProvider
      *
      * @var string
      */
-    public const HOME = '/dashboard';
+    public const HOME = '/dishes';
 
     /**
      * Define your route model bindings, pattern filters, and other route configuration.
