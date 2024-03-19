813f24cdc230d3246fd22b4acfaed7b4a412c844
Davide Prencipe <davidedev04@gmail.com>
2024-03-19 15:24:57 +0100

    checkpoint order(1)

--- front-end/src/components/Details.vue
+++ front-end/src/components/Details.vue
@@ -6,7 +6,8 @@ export default {
     data() {
         return {
             ristoranti: [], // Array per memorizzare i dati dei ristoranti
-            // tipologie: [],
+            orders: [],
+            totalPrice: 0
         };
     },
     mounted() {
@@ -45,6 +46,28 @@ export default {
         getImageUrl(ristorante) {
             return `http://localhost:8000/storage/${ristorante}`;
         },
+        addToOrder(dish) {
+            const existingOrder = this.orders.find(order => order.name === dish.name);
+                if (existingOrder) {
+                    existingOrder.quantity++;
+                    existingOrder.price += dish.price;
+                } else {
+                    this.orders.push({ name: dish.name, quantity: 1, price: dish.price });
+                }
+                this.totalPrice += dish.price; // Aggiorna il prezzo totale
+        },
+        removeFromOrder(dish) {
+            const existingOrderIndex = this.orders.findIndex(order => order.name === dish.name);
+                if (existingOrderIndex !== -1) {
+                    const existingOrder = this.orders[existingOrderIndex];
+                    existingOrder.quantity--;
+                    existingOrder.price -= dish.price;
+                if (existingOrder.quantity === 0) {
+                    this.orders.splice(existingOrderIndex, 1);
+                }
+                this.totalPrice -= dish.price; // Aggiorna il prezzo totale
+                }
+        }
     },
 };
 </script>
@@ -54,7 +77,10 @@ export default {
         <div class="arrow-container">
             <span class="back-arrow">
                 <!-- Evento click per tornare indietro -->
-                <i @click="goBack" class="fa-solid fa-circle-left my-4 ms-3"></i>
+                <i
+                    @click="goBack"
+                    class="fa-solid fa-circle-left my-4 ms-3"
+                ></i>
             </span>
             <span class="restaurants-zone">
                 <strong>Ristoranti</strong>
@@ -73,8 +99,8 @@ export default {
                             'images/'
                         )
                             ? getImageUrl(
-                                ristoranti[this.$route.params.index].image
-                            )
+                                  ristoranti[this.$route.params.index].image
+                              )
                             : ristoranti[this.$route.params.index].image
                     "
                     alt="Ristorante Image"
@@ -82,12 +108,15 @@ export default {
             </div>
             <div class="col-12 col-md-6 text-center text-md-start mb-4">
                 <h1>{{ ristoranti[this.$route.params.index].name }}</h1>
-                <div v-for="(type, index) in ristoranti[this.$route.params.index].tipologies" :key="index" class="my-2">
-                    <h4 class="text-info">
-                        - {{ type }}
-                    </h4>
+                <div
+                    v-for="(type, index) in ristoranti[this.$route.params.index]
+                        .tipologies"
+                    :key="index"
+                    class="my-2"
+                >
+                    <h4 class="text-info">- {{ type }}</h4>
                 </div>
-                
+
                 <h4 class="text-black-50">
                     {{ ristoranti[this.$route.params.index].address }}
                 </h4>
@@ -101,7 +130,12 @@ export default {
             </div>
             <div class="row">
                 <div class="col-12 col-md-8 text-center">
-                    <div v-for="(dish, index) in ristoranti[this.$route.params.index].dishes" :key="index">
+                    <div
+                        v-for="(dish, index) in ristoranti[
+                            this.$route.params.index
+                        ].dishes"
+                        :key="index"
+                    >
                         <div class="row my-5">
                             <div class="col-6 py-4 d-flex flex-column">
                                 <img :src="dish.image" alt="" />
@@ -109,15 +143,22 @@ export default {
                                 <span>{{ dish.description }}</span>
                                 <strong>{{ dish.price }}€</strong>
                             </div>
-                            <div class="col-6 d-flex justify-content-center align-items-center">
-                                <button type="button" class="mx-3 btn btn-outline-success">+</button>
-                                <button type="button" class="mx-3 btn btn-outline-danger">-</button>
+                            <div
+                                class="col-6 d-flex justify-content-center align-items-center"
+                            >
+                                <button type="button" class="mx-3 btn btn-outline-success" @click="addToOrder(dish)">+</button>
+                                <button type="button" class="mx-3 btn btn-outline-danger" @click="removeFromOrder(dish)">-</button>
                             </div>
                         </div>
                     </div>
                 </div>
-                <div class="card_payment col-12 col-md-4 text-center border">
+                <div class="card_payment col-12 col-md-4 text-center border pt-3">
                     <h2>I TUOI ORDINI</h2>
+                    <div v-for="(order, index) in orders" :key="index">
+                        <p>{{ order.name }} ( {{ order.quantity }} )  <strong>{{ order.price }}€</strong></p>
+                    </div>
+                    <p><strong>Totale: {{ totalPrice }}€</strong></p>
+                    <button class="btn btn-primary" type="button" style="width: 100%;">Effettua l'ordine</button>
                 </div>
             </div>
         </div>
