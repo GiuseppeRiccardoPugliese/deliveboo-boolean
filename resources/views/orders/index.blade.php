dd5f714c748048e46ab0faf3879f43948d8bcb7b
maurocazzato <maurizzio.125@gmail.com>
2024-03-22 09:29:49 +0100

    fix simbolo €

--- resources/views/orders/index.blade.php
+++ resources/views/orders/index.blade.php
@@ -27,7 +27,7 @@
                                         {{ $order->number_order }}
                                     </td>
                                     <td>
-                                        {{ $order->price }} <p>€</p>
+                                        {{ $order->price }} <span>€</span>
                                     </td>
                                     <td>
                                         {{ $order->guest_name }}
