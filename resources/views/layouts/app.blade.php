23a43e93ec672906e8d07b6b15eacd2b5ca10085
Giuseppe Riccardo Pugliese <giuseppe.riccardo.pugliese@gmail.com>
2024-03-26 12:50:20 +0100

    Change page title for back-end

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -8,7 +8,7 @@
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
-    <title>Deliveboo Back-End</title>
+    <title>Dashboard</title>
 
     <link rel="icon" type="image/png" href="{{ asset('favicon.png') }}">
     <!-- Fonts -->
