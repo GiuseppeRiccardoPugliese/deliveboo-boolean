d58cc89e7314c6ba626f0e3a0ca3040189c8798b
Giuseppe Riccardo Pugliese <giuseppe.riccardo.pugliese@gmail.com>
2024-03-27 10:54:43 +0100

    Fix footer on page & jumbo height

--- front-end/src/components/Home.vue
+++ front-end/src/components/Home.vue
@@ -11,27 +11,27 @@ export default {
     <!-- sezione dedicata al jumbotron -->
     <div class="container-fluid">
         <div class="jumbotron">
-            
-                <div class="jumbo-title">
-                    <span id="title-content"> I piatti che ami dove e quando vuoi </span>
-                </div>
-           
-            
-                <div class="advanced-search">
-                    <div class="advanced-input">
-                        <div>
-                            <span> Dai un'occhiata ai ristoranti nella città di 
-                                Milano <i class="fa-solid fa-location-dot"></i> 
-                            </span>
-                        </div>
-                        
-                        <!-- La rotta che tramite il cerca porta ai ristoranti -->
-                        <router-link :to="{ name: 'Restaurants' }" class="cerca">
-                            <a> <button type="button"> Cerca </button> </a>
-                        </router-link>
+
+            <div class="jumbo-title">
+                <span id="title-content"> I piatti che ami dove e quando vuoi </span>
+            </div>
+
+
+            <div class="advanced-search">
+                <div class="advanced-input">
+                    <div>
+                        <span> Dai un'occhiata ai ristoranti nella città di
+                            Milano <i class="fa-solid fa-location-dot"></i>
+                        </span>
                     </div>
+
+                    <!-- La rotta che tramite il cerca porta ai ristoranti -->
+                    <router-link :to="{ name: 'Restaurants' }" class="cerca">
+                        <a> <button type="button"> Cerca </button> </a>
+                    </router-link>
                 </div>
-            
+            </div>
+
 
         </div>
     </div>
@@ -41,6 +41,7 @@ export default {
 @use '../style/partials/variables' as *;
 
 .container-fluid {
+    min-height: 500px;
     width: 100%;
     background-image: url(../../public/img/jumbotron2.jpg);
     background-size: cover;
@@ -48,11 +49,11 @@ export default {
 
     .jumbotron {
         width: 100%;
-        height: 100%;
+        min-height: 500px;
         display: flex;
         align-items: center;
         padding: 30px;
-        background: linear-gradient(to right, rgba(0,0,0,1) 0%, rgba(0,0,0,0) 100%);
+        background: linear-gradient(to right, rgba(0, 0, 0, 1) 0%, rgba(0, 0, 0, 0) 100%);
 
         .jumbo-title {
             width: 50%;
@@ -60,43 +61,43 @@ export default {
             display: flex;
             justify-content: center;
             animation: slideInFromRight 1.2s ease forwards;
-            }
+        }
 
-            span {
-                color: white;
-                font-size: 80px;
-                font-weight: 600;
-                text-shadow: 5px 5px 200px black;
-            }
+        span {
+            color: white;
+            font-size: 80px;
+            font-weight: 600;
+            text-shadow: 5px 5px 200px black;
+        }
 
-            .advanced-search {
+        .advanced-search {
 
-                .advanced-input {
+            .advanced-input {
 
-                    display: flex;
-                    flex-direction: column;
-                    align-items: center;
+                display: flex;
+                flex-direction: column;
+                align-items: center;
 
-                    div {
-                    
-                        background-color: rgba(0, 0, 0, 0);
-                        margin-bottom: 30px;
+                div {
 
-                        span {
+                    background-color: rgba(0, 0, 0, 0);
+                    margin-bottom: 30px;
+
+                    span {
                         font-size: 20px;
                         // color: #ae3f68;
-                        }
                     }
-                    
+                }
 
-                    button {
-                        border-radius: 100px;
-                        padding: 12px 37px;
-                        color: #fff;
-                        background: #00ccbc;
-                        border: none;
-                        font-weight: 400;
-                    }
+
+                button {
+                    border-radius: 100px;
+                    padding: 12px 37px;
+                    color: #fff;
+                    background: #00ccbc;
+                    border: none;
+                    font-weight: 400;
+                }
 
                 input {
                     border-radius: 100px;
@@ -127,6 +128,7 @@ export default {
             transform: translateX(100%);
             opacity: 0;
         }
+
         100% {
             transform: translateX(0);
             opacity: 1;
@@ -137,21 +139,21 @@ export default {
 
 @media all and (min-width: 300px) and (max-width: 1024px) {
 
-    .jumbotron{
+    .jumbotron {
         flex-direction: column;
     }
 
-    .jumbo-title{
+    .jumbo-title {
         width: 100%;
         align-items: center;
         text-align: center;
-    
+
         #title-content {
-        font-size: 40px;
+            font-size: 40px;
+        }
     }
-    }
-   
-        
+
+
 }
 
 @media all and (min-width: 1025px) and (max-width: 1320px) {
@@ -160,19 +162,17 @@ export default {
         flex-direction: column;
     }
 
-    .jumbo-title{
+    .jumbo-title {
         width: 100%;
         align-items: center;
         padding: 10px;
 
-        #title-content  {
-        font-size: 40px;
-        text-align: center;
+        #title-content {
+            font-size: 40px;
+            text-align: center;
+        }
     }
-    }
-    
-        
+
+
 }
-
-
 </style>
