804ada46bad2440997f416dce84491f4da322f69
CosimoA <cosimo.archip@gmail.com>
2024-03-27 22:56:49 +0100

    FEAT: add modal to detail.vue component

--- front-end/src/components/Details.vue
+++ front-end/src/components/Details.vue
@@ -13,6 +13,8 @@ export default {
                 price: 0,
                 orders: [],
             },
+            showModalCart: false,
+            showModalDish: false,
         };
     },
     created() {
@@ -59,17 +61,8 @@ export default {
                 this.orderData.restaurantId = restaurantId;
                 this.orderData.restaurantIndex = restaurantIndex;
             } else if (this.orderData.restaurantId !== restaurantId) {
-                // Mostra un alert con il messaggio appropriato
-                if (confirm("Hai già un carrello aperto con un altro ristorante. Vuoi cancellarlo e proseguire o vuoi tornare sul vecchio ristorante?")) {
-                    // Se l'utente conferma, cancella l'ordine attuale e prosegui
-                    this.deleteOrders();
-                    this.orderData.restaurantId = restaurantId;
-                    this.orderData.restaurantIndex = restaurantIndex;
-                } else {
-                    // Altrimenti, torna sul vecchio ristorante
-                    this.$router.push({ name: 'Details', params: { index: this.orderData.restaurantIndex } });
-                    return;
-                }
+                this.openModalDish();
+                return;
             }
 
             const existingOrder = this.orderData.orders.find(order => order.name === dish.name);
@@ -105,16 +98,49 @@ export default {
             const currentRestaurantId = restaurantId;
 
             if (currentRestaurantId !== this.orderData.restaurantId) {
-                if (confirm("I piatti all'interno del carrello non appartengono a questo ristorante, confermi di voler ordinare i piatti del ristorante precedente?")) {
-                    this.$router.push({ name: 'Payment' });
-                } else {
-                    this.deleteOrders();
-                    return;
-                }
+                this.openModalCart()
             } else {
                 this.$router.push({ name: 'Payment' });
             }
+        },
+        // MODAL CART
+        openModalCart() {
+            this.showModalCart = true;
+            document.body.style.overflow = 'hidden';
+        },
+        closeModalCart() {
+            this.showModalCart = false;
+            document.body.style.overflow = '';
+        },
 
+        confirmOrderCart() {
+            this.$router.push({ name: 'Payment' });
+            this.closeModalCart();
+        },
+        cancelOrderCart() {
+            this.deleteOrders();
+            this.closeModalCart();
+        },
+        // MODAL DISH
+        openModalDish() {
+            this.showModalDish = true;
+            document.body.style.overflow = 'hidden';
+        },
+        closeModalDish() {
+            this.showModalDish = false;
+            document.body.style.overflow = '';
+        },
+        confirmModalDish() {
+            this.deleteOrders();
+            const restaurantId = this.ristoranti[this.$route.params.index].id;
+            const restaurantIndex = this.$route.params.index;
+            this.orderData.restaurantId = restaurantId;
+            this.orderData.restaurantIndex = restaurantIndex;
+            this.closeModalDish()
+        },
+        cancelModalDish() {
+            this.$router.push({ name: 'Details', params: { index: this.orderData.restaurantIndex } });
+            this.closeModalDish()
         },
         deleteOrders() {
             this.totalPrice = 0;
@@ -122,7 +148,7 @@ export default {
             this.orderData.restaurantId = "";
             this.orderData.price = 0;
             this.orderData.orders = [];
-        }
+        },
     },
     watch: {
         orders: { // Un watcher per monitorare le modifiche agli ordini e salvare nel localStorage
@@ -165,6 +191,7 @@ export default {
         </div>
 
         <div class="row mx-3 mb-3" v-if="ristoranti[this.$route.params.index]">
+
             <div class="col-12 col-md-6 d-flex justify-content-center align-items-center mb-4">
                 <img class="img_product rounded" :src="ristoranti[this.$route.params.index].image &&
                     ristoranti[this.$route.params.index].image.includes(
@@ -176,6 +203,7 @@ export default {
                     : ristoranti[this.$route.params.index].image
                     " alt="Ristorante Image" />
             </div>
+
             <div class="col-12 col-md-6 text-center text-md-start mb-4">
                 <h1 class="fw-bold">{{ ristoranti[this.$route.params.index].name }}</h1>
                 <div class="my-4 d-flex justify-content-center justify-content-md-start">
@@ -223,36 +251,57 @@ export default {
 
                     <h2>I TUOI ORDINI</h2>
 
-                    <div v-for="(order, index) in orderData.orders" :key="index">
-                        <p><strong class="text-black-50">x{{ order.quantity }} |</strong> {{ order.name }} <strong>{{
-                    order.price.toFixed(2) }}€</strong></p>
+                    <div class="dish-list" v-for="(order, index) in orderData.orders" :key="index">
+                        <div class="list">
+                            <p><strong class="text-black-50">x{{ order.quantity }} |</strong> {{ order.name }}
+                                <strong>{{
+                    order.price.toFixed(2) }}€</strong>
+                            </p>
+                        </div>
                     </div>
                     <p><strong>Totale: {{ totalPrice.toFixed(2) }}€</strong></p>
 
-                    <div v-if="totalPrice !== 0">
-                        <!-- <router-link :to="{ name: 'Payment' }" class="text-white"> -->
-                        <button class="btn btn-success" type="button" style="width: 100%;" @click="makeOrder()">
-                            Effettua l'ordine
-                        </button>
-                        <!-- </router-link> -->
-                    </div>
-
-                    <button v-else class="btn btn-success" type="button" style="width: 100%;" disabled
-                        data-bs-toggle="button">
-                        <!-- <router-link :to="{ name: 'Payment' }" class="text-white"> -->
+                    <button class="btn btn-success" type="button" style="width: 100%;" :disabled="totalPrice === 0"
+                        @click="totalPrice !== 0 && makeOrder()">
                         Effettua l'ordine
-                        <!-- </router-link> -->
                     </button>
 
-                    <button v-if="totalPrice !== 0" class="btn btn-danger mt-3" type="button" style="width: 100%;"
+                    <button class="btn btn-danger mt-3" type="button" style="width: 100%;" :disabled="totalPrice === 0"
                         @click="deleteOrders()">
                         Svuota il carrello
                     </button>
-                    <button v-else class="btn btn-danger mt-3" type="button" style="width: 100%;" disabled
-                        data-bs-toggle="button" @click="deleteOrders()">
-                        Svuota il carrello
-                    </button>
 
+
+                </div>
+
+                <!-- MODALS -->
+
+                <!-- Modal per quando il cliente vuole confermare l'ordine da un ristorante diverso rispetto ai piatti del carrello -->
+                <div v-if="showModalCart" class="modal-overlay">
+                    <div class="modal-content">
+                        <button class="close" @click="closeModalCart">✖</button>
+                        <img src="https://cdn-icons-png.flaticon.com/512/4290/4290854.png" alt="cookies-img" />
+                        <p>I piatti all'interno del carrello non appartengono a questo ristorante, vuoi confermare
+                            l'ordine?</p>
+                        <div class="btn-container" style="display: flex; justify-content: center;">
+                            <button class="accept" @click="confirmOrderCart">Conferma</button>
+                            <button class="decline" @click="cancelOrderCart">Annulla</button>
+                        </div>
+                    </div>
+                </div>
+
+                <!-- Modal per quando l'utente aggiunge piatti al carrello da ristoranti diversi -->
+                <div v-if="showModalDish" class="modal-overlay">
+                    <div class="modal-content">
+                        <button class="close" @click="closeModalDish">✖</button>
+                        <img src="https://cdn-icons-png.flaticon.com/512/4290/4290854.png" alt="cookies-img" />
+                        <p>Hai già un carrello aperto con un altro ristorante. Vuoi cancellarlo e proseguire oppure
+                            annulla?</p>
+                        <div class="btn-container" style="display: flex; justify-content: center;">
+                            <button class="accept" @click="confirmModalDish">Prosegui</button>
+                            <button class="decline" @click="cancelModalDish">Annulla</button>
+                        </div>
+                    </div>
                 </div>
             </div>
         </div>
@@ -260,6 +309,11 @@ export default {
 </template>
 
 <style lang="scss" scoped>
+body.modal-open {
+    overflow: hidden;
+    /* Impedisce lo scrolling */
+}
+
 .img_product {
     width: 100%;
     max-width: 700px;
@@ -289,11 +343,16 @@ export default {
 }
 
 .card_payment {
-    height: 600px;
+    height: 650px;
     background-color: #fff;
     position: sticky;
     top: 2%;
     box-shadow: 0 4px 10px rgba(0, 0, 0, 0.2);
+}
+
+.dish-list {
+    text-align: start;
+    padding-left: 6rem;
 }
 
 .bg_brown {
@@ -302,5 +361,96 @@ export default {
 
 .type_border {
     border-radius: 12px;
+}
+
+
+
+// MODAL
+
+.modal-overlay {
+    position: fixed;
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 100%;
+    backdrop-filter: blur(20px);
+    background-color: rgba(0, 0, 0, 0.3);
+    display: flex;
+    justify-content: center;
+    align-items: center;
+}
+
+.modal-content {
+    width: 80%;
+    max-width: 600px;
+    background-color: #fff;
+    color: #000;
+    text-align: center;
+    border-radius: 20px;
+    padding: 30px;
+}
+
+.modal-content button.close {
+    position: absolute;
+    top: 10px;
+    right: 10px;
+    width: 30px;
+    font-size: 20px;
+    color: #c0c5cb;
+    background-color: transparent;
+    border: none;
+    overflow: hidden;
+}
+
+.modal-content button.close:hover {
+    color: #3d3e40;
+}
+
+.modal-content img {
+    width: 100px;
+    // margin-bottom: 30px;
+    margin: 0 auto 30px;
+}
+
+.modal-content p {
+    margin-bottom: 30px;
+}
+
+.modal-content button.accept {
+    width: 130px;
+    background-color: #0d6efd;
+    border: none;
+    border-radius: 100px;
+    padding: 14px;
+    margin: 0 0.5rem;
+    font-size: 1rem;
+    color: white;
+    font-size: larger;
+    box-shadow: 0px 6px 18px -5px #0d6efd;
+}
+
+.modal-content button.accept:hover {
+    background-color: #0037ff;
+    box-shadow: 0px 6px 18px -5px #0037ff;
+
+}
+
+.modal-content button.decline {
+    width: 130px;
+    background-color: #ff5a5f;
+    border: none;
+    border-radius: 100px;
+    padding: 14px;
+    margin: 0 0.5rem;
+    font-size: 1rem;
+    color: white;
+    font-size: larger;
+    box-shadow: 0px 6px 18px -5px #ff5a5f;
+}
+
+.modal-content button.decline:hover {
+    background-color: #f82e34;
+    box-shadow: 0px 6px 18px -5px #f82e34;
+
 }
 </style>
